0108e86ab779ed698fd653defce393d19982392a
Francisco Afonso <a100691@alunos.uminho.pt>
2023-11-20 18:09:17 +0000

    Possible to ask list of files and for a file specification

--- FS_Node.py
+++ FS_Node.py
@@ -1,56 +1,80 @@
 import socket
 import sys
+import json
 
 import os
 
 def main():
-
     # Remove first argument (script name)
 	args = sys.argv[1:] # args = main_folder serverIP Port
-
-    # Open specified folder
+    # Open own folder
 	os.chdir(args[0])
 
-	# Not implemented TCP 9090 default port
-	HOST = args[1]
-	PORT = args[2]
-	
-    # Open socket
-	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	client_socket.connect((HOST, int(PORT)))
-	
-	
-	print('FS Transfer Protocol: à escuta na porta UDP 9090')
+	# Tracker information
+	SEND_HOST = args[1]
+	# Usage of try to implement port TCP 9090 as default port
+	try:
+		SEND_PORT = int(args[2])
+	except:
+		SEND_PORT = 9090
 
-	# Interaction while
+	LISTEN_HOST = 'localhost'
+	# UDP port to listen other Nodes
+	LISTEN_PORT = 9090
+	
+
+	# Client Interaction while
 	while True:
-		# Add 2 - Pedir lista de ficherios\n3 - Pedir localização de ficheiro\n
+		# Open TCP socket to interact with Tracker
+		client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		client_socket.connect((SEND_HOST, SEND_PORT))
+
+		# adicionar 2 - Pedir lista de ficherios\n3 - Pedir localização de ficheiro\n
 		i = input("Escolha uma operação:\n1 - Registar Node\n4 - Cancelar\n")
-		if (i == '1'):
+		if i == '1':
 			# Create message
-			message = "1;9090;file1.txt;file2.txt"
+			message = "1;9090;file1.txt;1,2;file2.txt;1,2"
+
+			# Send message
+			client_socket.sendall(message.encode('utf-8'))
+
+			# Receive and print response
+			data = client_socket.recv(1024)
+			print(f"Server response: {data.decode('utf-8')}")
+
 			break
 
-		# elif(i == '2'):
-		# 	print("Não implementado")
+		elif i == '2':
+			# Create message
+			message = "2"
 
-		# elif(i == '3'):
-		# 	print("Não implementado")
+			# Send message
+			client_socket.sendall(message.encode('utf-8'))
 
-		elif(i == '4'):
+			# Receive and print response
+			data = client_socket.recv(1024)
+
+			decoded_json = data.decode()
+			decoded_strings = json.loads(decoded_json)
+
+			print(f"Server response: {decoded_strings}")
+
+			message = "3;" + input("File position: ")
+			client_socket.sendall(message.encode('utf-8'))
+
+			data = client_socket.recv(1024)
+			decoded_json = data.decode()
+
+			print(f"Server response: {decoded_json}")
+			break
+
+		elif i == '4':
 			break
 
 		else:
 			print("Operação inválida!")
 
-    # Send message
-	client_socket.sendall(message.encode('utf-8'))
-
-    # Receive and print response
-	data = client_socket.recv(1024)
-	print(f"Server response: {data.decode('utf-8')}")
-
-    # Close socket
+	# Close socket
 	client_socket.close()
 
 if __name__ == "__main__":
--- Tracker/FS_Tracker.py
+++ Tracker/FS_Tracker.py
@@ -1,14 +1,16 @@
 import socket
 
 import csv
+import json
 
 
-HOST = '10.4.4.1'
+# HOST = '10.4.4.1'
+HOST = 'localhost'
 PORT = 9090
 
 store_file_path = "FS_Data.csv"
 
-# open socket
+# Open socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 
@@ -17,32 +19,62 @@ server_socket.listen()
 print(f"Server is listening on {HOST}:{PORT}")
 
 while True:
+    # Accept client connection
     client_socket, client_address = server_socket.accept()
-
-    print(f"FS Transfer Protocol: à escuta na porta {PORT}")
-
-
-    print(f"Client address = {client_address[0]}")
 
     data = client_socket.recv(1024)
     if not data:
         break
-   
-    received = data.decode('utf-8')
-    print(f"Received: {received}")
 
-    if (received[0] == '1'):
-        # Regist a new Node, isn't possible to regist a Node twice (remove or add files)
+    received = data.decode('utf-8')
+
+    if received[0] == '1':
         with open(store_file_path, 'a', newline='') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=';')
             csvwriter.writerow([client_address[0]] + received[2:].split(";"))
-            
-    else:
-        response = "Invalid Operation"
+        print(f"Nodo {client_address[0]} registado")
+        response = "Successfully registered: " + client_address[0]
+        client_socket.sendall(response.encode('utf-8'))
 
-    response = "Received your message: " + received
-    client_socket.sendall(response.encode('utf-8'))
-    
+    elif received[0] == '2':
+        this_dict = {}
+        with open(store_file_path, 'r', newline='') as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=';')
+            for row in csvreader:
+                i = 2
+                m = len(row)
+                while i < m:
+                    this_dict.setdefault(row[i], [])
+                    this_dict[row[i]].append((row[0], row[i+1]))
+                    i += 2
+        keys = list(this_dict.keys())
+        json_data = json.dumps(list(this_dict.keys()))
+        response = json_data.encode()
+        client_socket.sendall(response)
+
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        received = data.decode('utf-8')
+        index = int(received[2:])
+
+        if 0 <= index <= len(keys):
+            json_data = json.dumps(this_dict[keys[index]])
+
+            print("dict")
+            print(this_dict)
+            print("this_dict[index]") 
+            print(this_dict[keys[index]])
+
+            response = json_data.encode()
+            client_socket.sendall(response)
+
+    else:
+        response = "Operação inválida"
+        client_socket.sendall(response)
+        break #BREAK PARA FACILITAR TESTES
+
+
     client_socket.close()
     print(f"Closed client socket with {client_address}")
 
